6dd342840dcc173dd979acd8c2c369351ed6b1e9
Milan Rokad <milanrokad@gmail.com>
2022-01-15 15:26:07 +0530

    Revert "task1"
    This reverts commit 1e979c7f1e61a22d9da0b2539cfca64da5adece8.

--- README.md
+++
@@ -1,4 +0,0 @@
-project setup
-
-1. yarn install
-2. yarn dev
--- src/components/TrendAnalysis/helper.jsx
+++
@@ -1,7 +0,0 @@
-export const tableData = {
-	columns: ['User', 'Accuracy', 'Attempt', 'Score', 'Band'],
-	data: [
-		['You', '60%', 5, 6, 6],
-		['Talati Shlok (Topper)', '95%', 3, 8.5, 8.5],
-	],
-}
